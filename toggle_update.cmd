80614981e6c7cf68404f7743e4356772ba96325e
Kershan Naidoo <Kershan Naidoo>
2017-01-29 15:18:54 +0200

    Ask for a on or off rather then a yes or no

--- toggle_update.cmd
+++ toggle_update.cmd
@@ -1,19 +1,29 @@
 @ECHO OFF
-SET /P answer=Are you sure you want to toggle Windows Update Service? (y or n):
-IF "%answer%"=="y" (
+SET /P answer=Attempt to turn on Windows Update Service? (on or off):
+IF "%answer%"=="on" (
 	@ECHO ON
 
-	NET START wuauserv || NET STOP wuauserv
-	NET START bits || NET STOP bits
-	NET START dosvc || NET STOP dosvc
+	NET START wuauserv
+	NET START bits
+	NET START dosvc
 
 	@ECHO OFF
+) ELSE IF "%answer%"=="off" (
+	@ECHO ON
+
+	NET STOP wuauserv
+	NET STOP bits
+	NET STOP dosvc
+
+	@ECHO OFF
+) ELSE (
+	ECHO Please restart script and choose either yes or no.
 )
 SET /P answer=Do you want to restart now? (y or n):
 IF "%answer%"=="y" (
-	ECHO "Restarting in a minute!"
+	ECHO Restarting in a minute!
 	SHUTDOWN -r -t 60
 ) ELSE (
-	ECHO "Restart later to make sure the the update service is working correctly"
+	ECHO Restart later to make sure the the update service is working correctly
 )
 PAUSE
